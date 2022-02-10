2af1e22b76f242f98b716d5e8f9555a0f36e4c80
Marcelo Tosco @capynet <capy.net@gmail.com>
2022-02-10 08:00:54 +0100

    Cleaning old code.

--- js/pel.behavior.js
+++ js/pel.behavior.js
@@ -1,37 +1,12 @@
-// jshint ignore: start
 (function ($, Drupal) {
-  Drupal.behaviors.pel = {
+  Drupal.behaviors.plb = {
     attach: function (context, settings) {
       /////////////////
 
-      $('#pel-builder').once('pel').each(function (e) {
-        new LayoutBuilderWrapper(this, settings.nppe_pel.layout);
+      $('#pel-builder').once('plb').each(function (e) {
 
-        console.log('llega')
-        console.log(settings.nppe_pel.layout)
-
-        this.addEventListener('layout.save', function (e) {
-          console.log('Sale')
-          console.log(e.detail.jsonLayout)
-
-          const data = {
-            layout: e.detail.jsonLayout
-          };
-
-          $.post(settings.nppe_pel.path, data)
-            .done(function () {
-              location.reload();
-            })
-            .fail(function () {
-              // @todo catch this and show to the user.
-              console.error('The node does not exist.');
-            });
-        });
-
-      });
-
-      /////////////////
-    }
+        /////////////////
+      }
   };
 
-})(jQuery, Drupal);
+  }) (jQuery, Drupal);
