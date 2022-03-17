a72e9e00ac278d8365bf9941d2232585775dfb4d
Marcelo Tosco @capynet <capy.net@gmail.com>
2022-03-17 16:53:34 +0100

    More module name changes.

---
+++ js/merlb.behavior.js
@@ -0,0 +1,12 @@
+(function($, Drupal) {
+        Drupal.behaviors.merlb = {
+            attach: function(context, settings) {
+                /////////////////
+
+                $('#merlb-builder').once().each(function(e) {
+
+                        /////////////////
+                    }
+                };
+
+            })(jQuery, Drupal);
